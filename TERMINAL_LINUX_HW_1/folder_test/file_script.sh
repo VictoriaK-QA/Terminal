b37518243c4fe6fd8a53d4715a6dd0cfded85838
VictoriaK-QA <v903971@gmail.com>
2023-03-14 17:54:30 +0300

    folder

---
+++ TERMINAL_LINUX_HW_1/folder_test/file_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+cd Terminal_dz1
+mkdir folder1 folder2 folder3
+cd folder3
+touch file1.txt file2.txt file3.txt file4.json file5.json
+mkdir folder_dz1 folder_dz2 folder_dz3
+ls -la 
+mv file3.txt file4.json ~/folder_test/Terminal_dz1/folder1/
